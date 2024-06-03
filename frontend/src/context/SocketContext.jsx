254cc7d0af5564c4669da6edc934b6e6201a31da
Aniket-1020 <113438494+Aniket-1020@users.noreply.github.com>
2024-06-03 16:17:24 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 	const user = useRecoilValue(userAtom);
 
 	useEffect(() => {
-		const socket = io("http://localhost:5000", {
+		const socket = io("https://social-media-1-36bl.onrender.com/chat", {
 			query: {
 				userId: user?._id,
 			},
